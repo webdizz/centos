0a009fe9a657c956b3b86f3f1f6a80fd489ed794
Izzet Mustafaiev <webdizz@gmail.com>
2016-02-08 12:22:10 +0200

    Add installation of Docker Compose and bash complutions

--- script/docker.sh
+++ script/docker.sh
@@ -19,3 +19,14 @@ echo "==> Starting docker"
 service docker start
 echo "==> Enabling docker to start on reboot"
 chkconfig docker on
+
+curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/local/bin/dc
+
+ yum install -y htop bash-completion
+ curl -L https://raw.githubusercontent.com/docker/docker/master/contrib/completion/bash/docker > /etc/bash_completion.d/docker
+ curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
+ curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/bash/docker-compose > /etc/bash_completion.d/dc
+
+ sed -i -E "s/_docker_compose [^)]+/_docker_compose dc/g" /etc/bash_completion.d/dc
